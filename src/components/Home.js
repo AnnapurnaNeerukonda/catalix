5e52e0e89e1dbc99fcebff9bcef9123b09a0149f
AnnapurnaNeerukonda <22b01a12b3@svcw.edu.in>
2024-04-08 19:17:46 +0530

    cards added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,103 @@
 import React from 'react';
 import './Home.css';
 import { Carousel, Card, InputGroup, FormControl, Button } from 'react-bootstrap';
-import { BiSearch, BiX } from 'react-icons/bi'; // Add this import
+import { BiSearch, BiX } from 'react-icons/bi';
 
 export default function Home() {
   return (
     <div className='container'>
       <div className='row'>
-      <div className='col-9 mx-auto '>
+      <div className='col-md-6 mt-5 m-md-0'>
+          <div className='col-9 mx-auto mb-4'>
             <InputGroup>
               <InputGroup.Text><BiSearch /></InputGroup.Text>
-              <FormControl placeholder="Search..." />   
+              <FormControl placeholder="Search by profile,setting,..." />   
               <InputGroup.Text><BiX /></InputGroup.Text>
             </InputGroup>
           </div>
-        <div className='col-md-7'>
           <h3>Set-up catalix for Success</h3>
           <p>Get Catalix up to date or start your transformation by following the guide below</p>
-          <Carousel className="custom-carousel">
-            <Carousel.Item>
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                <button className="btn btn-primary">Button</button>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <button className="btn btn-primary">Button</button>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                <button className="btn btn-primary">Button</button>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </div>
+          <div className='cards'>
+            <Carousel className='custom-carousel' indicators={false}>
+              <Carousel.Item>
+                <Card className='card'>
+                  <Card.Body>
+                    <Card.Title>1/6</Card.Title>
+                    <Card.Text>
+                      <p className='description'> Build Your development value stream map</p>
+                      <button className='btn'>Start Building</button>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Card className='card'>
+                  <Card.Body>
+                    <Card.Title>2/6</Card.Title>
+                    <Card.Text>
+                      <p className='description'> Build Your development value stream map</p>
+                      <button className='btn'>Start Building</button>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Card className='card'>
+                  <Card.Body>
+                    <Card.Title>3/6</Card.Title>
+                    <Card.Text>
+                      <p className='description'> Build Your development value stream map</p>
+                      <button className='btn'>Start Building</button>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Card className='card'>
+                  <Card.Body>
+                    <Card.Title>4/6</Card.Title>
+                    <Card.Text>
+                      <p className='description'> Build Your development value stream map</p>
+                      <button className='btn'>Start Building</button>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Card className='card'>
+                  <Card.Body>
+                    <Card.Title>5/6</Card.Title>
+                    <Card.Text>
+                      <p className='description'> Build Your development value stream map</p>
+                      <button className='btn'>Start Building</button>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Card className='card'>
+                  <Card.Body>
+                    <Card.Title>6/6</Card.Title>
+                    <Card.Text>
+                      <p className='description'> Build Your development value stream map</p>
+                      <button className='btn'>Start Building</button>
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer><h3 className='textCenter'>......</h3></Card.Footer>
+                </Card>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </div>
         <div className='col-md-5'>
           <div className='d-flex justify-content-end'>
             <i className="bi bi-bell me-2 fs-5"></i>
             <i className="bi bi-chat-left-dots me-2 fs-5"></i>
           </div>
-          <h3>Get ready to transform</h3>
+          <h3 className='mb-4 mt-4'>Get ready to transform</h3>
+         
           <p>Here's a video to get started. As your transformation progresses, you'll get fresh tips and insights here</p>
-          <iframe width="350" height="200" src="https://www.youtube.com/embed/8qwnFEFmDNY" title="How to be change ready | Neil Walker | TEDxWarsawWomen" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe width="400" height="250" src="https://www.youtube.com/embed/8qwnFEFmDNY" title="How to be change ready | Neil Walker | TEDxWarsawWomen" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
       </div>
     </div>
